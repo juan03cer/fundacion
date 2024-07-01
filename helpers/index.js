98be5be1205142469eb46592d0dc467a7ea95c13
Dsm-Montalvo <al222210568@gmail.com>
2024-06-30 20:18:38 -0600

    eliminacion

--- helpers/index.js
+++
@@ -1,9 +0,0 @@
- const esUsuario = (usuarioid, pacienteUsuarioid)=>{
-    return usuarioid=== pacienteUsuarioid
- }
-
-
- export {
-    esUsuario,
-    
- }
--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./views/**/*.pug'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
