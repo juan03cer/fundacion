643353ce9860fd66219d288f066dccc7c7776308
juan03cer <juan03esteban@hotmail.com>
2024-07-11 19:26:03 -0600

    arreglo de edad

--- public/js/calcularEdad.js
+++ public/js/calcularEdad.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const fechaNacimientoInput = document.getElementById('fechanacimiento');
-    const edadInput = document.getElementById('edad');
-    const mesesInput = document.getElementById('meses');
-  
-    fechaNacimientoInput.addEventListener('change', function() {
+  const fechaNacimientoInput = document.getElementById('fechanacimiento');
+  const edadInput = document.getElementById('edad');
+  const mesesInput = document.getElementById('meses');
+
+  fechaNacimientoInput.addEventListener('change', function() {
       const fechaNacimiento = new Date(fechaNacimientoInput.value);
       const hoy = new Date();
       
       let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
-      const mes = hoy.getMonth() - fechaNacimiento.getMonth();
-  
+      let mes = hoy.getMonth() - fechaNacimiento.getMonth();
+
       if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
-        edad--;
+          edad--;
+          mes = (mes + 12) % 12;
       }
-  
-      const meses = mes < 0 ? (12 + mes) : mes;
-  
+
       edadInput.value = edad;
-      mesesInput.value = meses;
-    });
+      mesesInput.value = mes;
   });
-  
+});
